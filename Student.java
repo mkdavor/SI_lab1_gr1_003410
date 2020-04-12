c50dbf8ec1be3019c8f01b5aee7308ceac0253e5
ddenikj <davor.denikj@redheads.de>
2020-04-12 12:35:37 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,18 +1,55 @@
+import java.util.stream.IntStream;
+
 class Student {
 	String index;
 	String firstName;
 	String lastName;
 	int[] labPoints;
 	
-	//TODO constructor
+	public Student () {
+		
+	}
 
-	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int[] getLabPoints() {
+		return labPoints;
+	}
+
+	public void setLabPoints(int[] labPoints) {
+		this.labPoints = labPoints;
+	}
 
 	public double getAverage() {
-		//TODO
+		int sum = IntStream.of(labPoints).sum();
+		return (double)sum / labPoints.length;
 	}
 
 	public boolean hasSignature() {
-		//TODO
+		return labPoints.length > 8;
 	}
+
+	
 }
